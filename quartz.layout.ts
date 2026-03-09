74f6c29fc23f90aa6adeee03038526d9f8a6535e
Alex Skryl <rut216@gmail.com>
2026-03-09 13:46:42 -0500

    Claude/setup research repo structure (#27)

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -32,9 +32,9 @@ export const defaultContentPageLayout: PageLayout = {
     Component.Explorer(),
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
+    Component.Graph(),
   ],
 }
 
